4df6f4ffe0b0af63ceaaca27604e02245fe21b4a
Fire-sleg <fireslegstars@gmail.com>
2023-06-06 16:24:34 +0300

    add sth

---
+++ Pages/Bosses/RootPack.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CupHeadWebApp.Pages.Bosses
+{
+    public class RootPackModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -15,3 +15,18 @@
 //    // document.getElementById("ad-container").style.visibility = "visible";
 //    document.getElementById("ad-container").style.opacity = 1;
 //}
+
+
+function slowScroll(id) {
+    $("html, body").animate(
+        {
+            scrollTop: $(id).offset().top,
+        },
+        1000
+    );
+}
+
+$(document).on("scroll", function () {
+    if ($(window).scrollTop() === 0) $("header").removeClass("fixed");
+    else $("header").attr("class", "fixed");
+});

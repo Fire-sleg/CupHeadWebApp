fbcd4113b811dc5f304653809054306398f0abfc
Fire-sleg <fireslegstars@gmail.com>
2023-06-06 09:35:49 +0300

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+//function adCloseButton() {
+//    document.getElementById("ad-container").style.opacity = 0;
+//    setTimeout(function () {
+//        document.getElementById("ad-container").style.display = "none";
+//    }, 500);
+//    // document.getElementById("ad-container").style.display = "none";
+//    document.body.style.opacity = 1;
+//}
+
+//function showAd() {
+//    // document.getElementById("ad-container").style.visibility = "visible";
+//    document.getElementById("ad-container").style.opacity = 1;
+//}
